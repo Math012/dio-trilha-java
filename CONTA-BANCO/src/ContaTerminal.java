b969a148373d160c373aa1add06cba7b9c8b46d3
Math012 <math111.0811@gmail.com>
2025-02-04 19:10:01 -0300

    Implementando a ContaService, lendo e imprimindo os dados do usuario

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import service.ContaService;
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        ContaService contaService = new ContaService();
+
+
+        System.out.println("Por favor, digite o número ! ");
+        contaService.setNumero(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência ! ");
+        contaService.setAgencia(scanner.nextLine());
+
+        System.out.println("Por favor, digite o seu nome ! ");
+        contaService.setNomeCliente(scanner.nextLine());
+
+
+        System.out.println("Por favor, digite o seu saldo ! ");
+        contaService.setSaldo(scanner.nextDouble());
+
+        System.out.println(("Olá "
+                + contaService.getNomeCliente()
+                + ", obrigado por criar uma conta em nosso banco,"
+                + " sua agência é "
+                + contaService.getAgencia()
+                + ", conta "+contaService.getNumero()
+                +" e seu saldo "+contaService.getSaldo()
+                +" já está disponível para saque"));
     }
 }
